92e5cbf132b55abbd2bb755073981380c34288d3
DESKTOP-S2C7CJG\computer <kys@greenit.cc>
2023-01-05 08:46:20 +0900

    first commit

---
+++ src/main/java/com/example/projectU/ProjectUApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectUApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectUApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projectU/ProjectUApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectU;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectUApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
